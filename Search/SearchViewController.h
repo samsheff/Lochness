0c411586c12aed923962fbb2cb812e4498f13560
Sam Sheffres <ssheffres@gmail.com>
2014-12-31 10:49:56 +0600

    Rebrand as lochness

---
+++ Search/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Search
+//
+//  Created by Sam Sheffres on 12/30/14.
+//  Copyright (c) 2014 Sam Sheffres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
